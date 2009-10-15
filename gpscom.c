11d9490e978ea778d6a3e31bb831e77688f58c14
znorris <znorris@aeolus.eece.labs>
2009-10-15 16:48:16 -0400

    im not sure what to do with the data once we have it

--- gpscom.c
+++ gpscom.c
@@ -9,22 +9,41 @@
 #include <stdio.h>
 #include <avr/io.h>
 #include <avr/iom8.h>
+#include <string.h>
 
 #define BAUD 4800
 #define MSGLENGTH 70
+#define PLENGTH 10
 
 void init_serial(void);
 void print(char * string);
 void tx_str(char data);
 char rx_byte(void);
 char* rx_str(void);
+int logdata(char * data, struct gps_loc* loc);
+
+struct gps_loc {
+	float time;
+	char status;
+	float lat;
+	char ns;
+	float lon;
+	char ew;
+	float sog;
+	float cog;
+	int date;
+};
 
 int main(int argc, char* argv[])
 {
 	char data[MSGLENGTH];
+	struct gps_loc gps;
 	
 	tx_str("$PSRF103,4,0,1,0");		//set up GPS to send in RMC format every 1 second
-	data = 
+	while(1) {
+		data = rx_str();		//get some data
+		logdata(data,&loc);		//log it
+	}
 }
 
 void init_serial(void)
@@ -43,7 +62,7 @@ void tx_byte(char data)
 
 void tx_str(char * string)
 {
-	while(*string != '\0') {
+	while(*string != '\0') {		//transmit the string byte by byte
 		tx_byte(*string);
 		string++;
 	}
@@ -51,10 +70,9 @@ void tx_str(char * string)
 
 char rx_byte(void)
 {
-	// Wait for data to be received 
-	while ( !(UCSRA & (1<<RXC)) );
-	// Get and return received data from buffer
-	return UDR;
+	while ( !(UCSRA & (1<<RXC)) );		// Wait for data to be received
+	return UDR;				// Get and return received data from buffer
+	
 }
 
 char* rx_str(void)
@@ -62,8 +80,63 @@ char* rx_str(void)
 	char str[MSGLENGTH];
 	int i = 0;
 	
-	do{
+	do{					//receive the output byte by byte
 		str[i] = rx_byte();
 		i++;
-	} while(str[i - 1] != '\0');
+	} while (str[i - 1] != '\0');
+	
+	return str;
+}
+
+int logdata(char * data, struct gps_loc* loc)
+{
+	int i = 0;
+	int j;
+	int field = 0;
+	char temp[PLENGTH];
+	
+	i = 0;
+	
+	while (data[i] != '\0') {		//while still in the string
+		while (data[i] != ',') {	//field by field in a comma separated file
+			temp[j] = data[i];	//copy to temp
+			i++;
+			j++;
+		}
+		j = 0;
+		switch (field) {
+			case 0:			//error checking, return -1 if wrong data
+				if (strcmp("$GPRMC",temp)) return -1;
+				break;
+			case 1:			//then fill in all the fields of the gps location
+				loc.time = atof(temp);
+				break;
+			case 2:
+				loc.status = *temp;
+				break;
+			case 3:
+				loc.lat = atof(temp);
+				break;
+			case 4:
+				loc.ns = *temp;
+				break;
+			case 5:
+				loc.lon = atof(temp);
+				break;
+			case 6:
+				loc.ew = *temp;
+				break;
+			case 7:
+				loc.sog = atof(temp);
+				break;
+			case 8:
+				loc.cog = atof(temp);
+				break;
+			case 9:
+				loc.date = atoi(temp);
+				break;
+		}
+		field++;
+	}
+	return 0;
 }
