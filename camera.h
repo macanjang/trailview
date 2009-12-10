8c8b4c927e88f29f7bc12028989b3c90e1e8c6f7
craigrun <craigrun@localhost>
2009-12-10 09:48:28 -0500

    camera works!

--- camera.h
+++ camera.h
@@ -8,11 +8,9 @@
 void camera_init(void);
 void camera_takephoto(const char * fname);
 void camera_txbyte(char c);
-inline char camera_rxbyte(void);
 char camera_response(void);
 void camera_rcv_cmd(char * cmdbuf);
+void camera_rcv(unsigned char * buf, int n);
 void camera_snd_cmd(char b2, char b3, char b4, char b5, char b6);
-void camera_snd_sync(void);
-void camera_rcv_ack(void);
 
 #endif
